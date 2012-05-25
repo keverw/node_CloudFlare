bdf6d326c659f33fb5e2e17ff739f362c343e4a5
Kevin <keverw92@gmail.com>
2012-05-25 02:09:28 -0400

    Beautifyed and http://www.jshint.com/

--- node_CloudFlare.js
+++ node_CloudFlare.js
@@ -1,9 +1,9 @@
 (function ()
 {
-    var isEmpty = function (obj)
-        {
-            return Object.keys(obj).length === 0;
-        }
+    function isEmpty(obj)
+    {
+        return Object.keys(obj).length === 0;
+    }
 
     var ranges = {};
 
@@ -34,32 +34,32 @@
         else
         {
             var range_check = require('range_check');
-			if (range_check.vaild_ip(ip_address))
-			{
-				var ip_ver = range_check.ver(ip_address);
-				if (ip_ver == 4)
-				{
-					return range_check.in_range(ip_address, ranges.v4)
-				}
-				else if (ip_ver == 6)
-				{
-					return range_check.in_range(ip_address, ranges.v6)
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else
-			{
-				return false;
-			}
+            if (range_check.vaild_ip(ip_address))
+            {
+                var ip_ver = range_check.ver(ip_address);
+                if (ip_ver === 4)
+                {
+                    return range_check.in_range(ip_address, ranges.v4);
+                }
+                else if (ip_ver === 6)
+                {
+                    return range_check.in_range(ip_address, ranges.v6);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 
     function get(req)
     {
-		var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+        var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
         if (typeof req.headers['cf-connecting-ip'] === 'undefined')
         {
             return ip_address;
@@ -69,7 +69,7 @@
             return req.headers['cf-connecting-ip'];
         }
     }
-	
+
     // Export public API
     var node_CloudFlare = {};
 
