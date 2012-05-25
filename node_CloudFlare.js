1cd1d14dd6ba43b61fde70e4cec46fde08463e4f
Kevin <keverw92@gmail.com>
2012-05-25 00:59:45 -0400

    Added range checking

--- node_CloudFlare.js
+++ node_CloudFlare.js
@@ -26,6 +26,7 @@
 
     function check(req)
     {
+		console.log(ranges);
         var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
         if (typeof req.headers['cf-connecting-ip'] === 'undefined')
         {
@@ -33,7 +34,27 @@
         }
         else
         {
-            //Do more checking.
+            var range_check = require('range_check');
+			if (range_check.vaild_ip(ip_address))
+			{
+				var ip_ver = range_check.ver(ip_address);
+				if (ip_ver == 4)
+				{
+					return range_check.in_range(ip_address, ranges.v4)
+				}
+				else if (ip_ver == 6)
+				{
+					return range_check.in_range(ip_address, ranges.v6)
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
         }
     }
 
