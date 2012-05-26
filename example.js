10ed6ea6efe24a3b56bbee361d7c69fdcf274d7f
Kevin <keverw92@gmail.com>
2012-05-25 20:50:11 -0400

    Reindented code to look better.

--- example.js
+++ example.js
@@ -5,14 +5,15 @@ var cf = require('./node_CloudFlare.js');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (req, res)
 {
-	if (cf.check(req)) //CF
+    if (cf.check(req)) //CF
     {
-		req.real_ip = cf.get(req);
-		req.connection.__defineGetter__('remoteAddress', function() {
-			return req.real_ip;
-		});
-	}
-	
+        req.real_ip = cf.get(req);
+        req.connection.__defineGetter__('remoteAddress', function ()
+        {
+            return req.real_ip;
+        });
+    }
+
     if (req.url === '/favicon.ico')
     { //Thank's https://gist.github.com/763822
         res.writeHead(200, {
@@ -26,16 +27,16 @@ var server = http.createServer(function (req, res)
         "Content-Type": "text/plain"
     });
 
-	var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-	res.end('Your IP: ' + ip_address);
+    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+    res.end('Your IP: ' + ip_address);
 });
 
 cf.load(function (error, fs_error)
 {
-	if (fs_error)
-	{
-		throw new Error(fs_error);
-	}
+    if (fs_error)
+    {
+        throw new Error(fs_error);
+    }
     server.listen(8880);
-	console.log('Server running.');
+    console.log('Server running.');
 });
