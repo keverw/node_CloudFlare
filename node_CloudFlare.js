50c88d12db2ee10870554a4e5a51d46c49abc876
keverw <kevinwhitman@Kevins-MacBook-Pro.local>
2015-10-26 05:06:51 -0400

    Removed (function () { and }()); in attempt to fix issue #1

--- node_CloudFlare.js
+++ node_CloudFlare.js
@@ -1,81 +1,79 @@
-(function ()
+
+function isEmpty(obj)
 {
-    function isEmpty(obj)
-    {
-        return Object.keys(obj).length === 0;
-    }
+    return Object.keys(obj).length === 0;
+}
 
-    var ranges = {};
+var ranges = {};
 
-    function load(callback) //Load the config
+function load(callback) //Load the config
+{
+    var fs = require('fs');
+    fs.readFile(__dirname + '/ranges.json', 'utf8', function (err, data)
     {
-        var fs = require('fs');
-        fs.readFile(__dirname + '/ranges.json', 'utf8', function (err, data)
+        if (err)
         {
-            if (err)
-            {
-                callback(true, err);
-            }
-            else
-            {
-                ranges = JSON.parse(data);
-                callback(false, null);
-            }
-        });
-    }
-
-    function check(req)
-    {
-        var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-        if (typeof req.headers['cf-connecting-ip'] === 'undefined')
-        {
-            return false;
+            callback(true, err);
         }
         else
         {
-            var range_check = require('range_check');
-            if (range_check.vaild_ip(ip_address))
+            ranges = JSON.parse(data);
+            callback(false, null);
+        }
+    });
+}
+
+function check(req)
+{
+    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+    if (typeof req.headers['cf-connecting-ip'] === 'undefined')
+    {
+        return false;
+    }
+    else
+    {
+        var range_check = require('range_check');
+        if (range_check.vaild_ip(ip_address))
+        {
+            var ip_ver = range_check.ver(ip_address);
+            if (ip_ver === 4)
             {
-                var ip_ver = range_check.ver(ip_address);
-                if (ip_ver === 4)
-                {
-                    return range_check.in_range(ip_address, ranges.v4);
-                }
-                else if (ip_ver === 6)
-                {
-                    return range_check.in_range(ip_address, ranges.v6);
-                }
-                else
-                {
-                    return false;
-                }
+                return range_check.in_range(ip_address, ranges.v4);
+            }
+            else if (ip_ver === 6)
+            {
+                return range_check.in_range(ip_address, ranges.v6);
             }
             else
             {
                 return false;
             }
         }
-    }
-
-    function get(req)
-    {
-        var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-        if (typeof req.headers['cf-connecting-ip'] === 'undefined')
-        {
-            return ip_address;
-        }
         else
         {
-            return req.headers['cf-connecting-ip'];
+            return false;
         }
     }
+}
 
-    // Export public API
-    var node_CloudFlare = {};
+function get(req)
+{
+    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+    if (typeof req.headers['cf-connecting-ip'] === 'undefined')
+    {
+        return ip_address;
+    }
+    else
+    {
+        return req.headers['cf-connecting-ip'];
+    }
+}
 
-    node_CloudFlare.load = load;
-    node_CloudFlare.check = check;
-    node_CloudFlare.get = get;
+// Export public API
+var node_CloudFlare = {};
 
-    module.exports = node_CloudFlare;
-}());
+node_CloudFlare.load = load;
+node_CloudFlare.check = check;
+node_CloudFlare.get = get;
+
+module.exports = node_CloudFlare;
