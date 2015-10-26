f3645af7fe6c0358b3a2e15301162f7b4dcae789
keverw <kevinwhitman@Kevins-MacBook-Pro.local>
2015-10-26 05:14:06 -0400

    indenting to tabs and IP range updated for issue #2

--- example.js
+++ example.js
@@ -4,38 +4,38 @@ var cf = require('./node_CloudFlare.js');
 // Configure our HTTP server to respond with the users IP address.
 var server = http.createServer(function (req, res)
 {
-    if (cf.check(req)) //CF
-    {
-        req.real_ip = cf.get(req);
-        req.connection.__defineGetter__('remoteAddress', function ()
-        {
-            return req.real_ip;
-        });
-    }
+	if (cf.check(req)) //CF
+	{
+		req.real_ip = cf.get(req);
+		req.connection.__defineGetter__('remoteAddress', function ()
+		{
+			return req.real_ip;
+		});
+	}
 
-    if (req.url === '/favicon.ico')
-    { //Thank's https://gist.github.com/763822
-        res.writeHead(200, {
-            'Content-Type': 'image/x-icon'
-        });
-        res.end();
-        return;
-    }
+	if (req.url === '/favicon.ico')
+	{ //Thank's https://gist.github.com/763822
+		res.writeHead(200, {
+			'Content-Type': 'image/x-icon'
+		});
+		res.end();
+		return;
+	}
 
-    res.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
+	res.writeHead(200, {
+		"Content-Type": "text/plain"
+	});
 
-    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-    res.end('Your IP: ' + ip_address);
+	var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+	res.end('Your IP: ' + ip_address);
 });
 
 cf.load(function (error, fs_error) //Loads the ranges and then starts the webserver.
 {
-    if (fs_error)
-    {
-        throw new Error(fs_error);
-    }
-    server.listen(8880);
-    console.log('Server running.');
+	if (fs_error)
+	{
+		throw new Error(fs_error);
+	}
+	server.listen(8880);
+	console.log('Server running.');
 });
--- node_CloudFlare.js
+++ node_CloudFlare.js
@@ -1,72 +1,72 @@
 
 function isEmpty(obj)
 {
-    return Object.keys(obj).length === 0;
+	return Object.keys(obj).length === 0;
 }
 
 var ranges = {};
 
 function load(callback) //Load the config
 {
-    var fs = require('fs');
-    fs.readFile(__dirname + '/ranges.json', 'utf8', function (err, data)
-    {
-        if (err)
-        {
-            callback(true, err);
-        }
-        else
-        {
-            ranges = JSON.parse(data);
-            callback(false, null);
-        }
-    });
+	var fs = require('fs');
+	fs.readFile(__dirname + '/ranges.json', 'utf8', function (err, data)
+	{
+		if (err)
+		{
+			callback(true, err);
+		}
+		else
+		{
+			ranges = JSON.parse(data);
+			callback(false, null);
+		}
+	});
 }
 
 function check(req)
 {
-    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-    if (typeof req.headers['cf-connecting-ip'] === 'undefined')
-    {
-        return false;
-    }
-    else
-    {
-        var range_check = require('range_check');
-        if (range_check.vaild_ip(ip_address))
-        {
-            var ip_ver = range_check.ver(ip_address);
-            if (ip_ver === 4)
-            {
-                return range_check.in_range(ip_address, ranges.v4);
-            }
-            else if (ip_ver === 6)
-            {
-                return range_check.in_range(ip_address, ranges.v6);
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
-        }
-    }
+	var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+	if (typeof req.headers['cf-connecting-ip'] === 'undefined')
+	{
+		return false;
+	}
+	else
+	{
+		var range_check = require('range_check');
+		if (range_check.vaild_ip(ip_address))
+		{
+			var ip_ver = range_check.ver(ip_address);
+			if (ip_ver === 4)
+			{
+				return range_check.in_range(ip_address, ranges.v4);
+			}
+			else if (ip_ver === 6)
+			{
+				return range_check.in_range(ip_address, ranges.v6);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
 
 function get(req)
 {
-    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-    if (typeof req.headers['cf-connecting-ip'] === 'undefined')
-    {
-        return ip_address;
-    }
-    else
-    {
-        return req.headers['cf-connecting-ip'];
-    }
+	var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+	if (typeof req.headers['cf-connecting-ip'] === 'undefined')
+	{
+		return ip_address;
+	}
+	else
+	{
+		return req.headers['cf-connecting-ip'];
+	}
 }
 
 // Export public API
